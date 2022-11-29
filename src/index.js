0268e11094b9cfed12a39fd9ab822045f602a1d9
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-29 19:54:58 -0300

    build: connecting to mongodb atlas

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+try {
+    await client.connect();
+    console.log("DB connected");
+} catch (error) {
+    console.log(error);
+}
+
+app.listen(5000);
