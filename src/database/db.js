01e51842ba7b8856b6ac2b3d77edb15d78be107c
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-30 00:52:22 -0300

    feat: creating database

---
+++ src/database/db.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+try {
+    await client.connect();
+    console.log("DB connected");
+} catch (error) {
+    console.log(error);
+}
+
+const db = client.db("dreanPoll");
+
+export const pollsCollection = db.collection("polls");
+export const choicesCollection = db.collection("choices");
--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-const client = new MongoClient(process.env.MONGO_URI);
-
-try {
-    await client.connect();
-    console.log("DB connected");
-} catch (error) {
-    console.log(error);
-}
-
 const port = process.env.PORT || 5000;
 
 app.listen(port);
